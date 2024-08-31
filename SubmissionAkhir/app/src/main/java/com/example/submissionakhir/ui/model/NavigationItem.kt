dae3bed09278a0a17e81157068fed88cc7f77482
RAYNF <rayhannoor33@gmail.com>
2024-08-31 23:04:00 +0700

    bug tidak bisa run starter project

--- SubmissionAkhir/app/src/main/java/com/example/submissionakhir/ui/model/NavigationItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.submissionakhir.ui.model
-
-import androidx.compose.ui.graphics.vector.ImageVector
-import com.example.submissionakhir.ui.navigation.Screen
-
-data class NavigationItem(val title:String,val icon:ImageVector,val screen: Screen)
