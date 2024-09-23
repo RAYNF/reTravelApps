f2505e9728c7c160179fbf082743d463ef76187c
RAYNF <rayhannoor33@gmail.com>
2024-09-23 17:18:08 +0700

    error obfuscation

---
+++ obfuscation/a165-android-expert-project-master/core/proguard-rules.pro
@@ -0,0 +1,17 @@
+-dontwarn java.lang.invoke.StringConcatFactory
+
+-keep class com.di.** { *; }
+-keepclassmembers class com.di.** { *; }
+
+-keep class com.domain.usecase.** { *; }
+-keepclassmembers class com.domain.usecase.** { *; }
+
+-keep class com.databinding.** { *; }
+-keepclassmembers class com.databinding.** { *; }
+
+-keep class com.ui.** { *; }
+-keepclassmembers class com.ui.** { *; }
+
+-keep class com.data.** { *; }
+-keepclassmembers class come.data.** { *; }
+
---
+++ obfuscation/a165-android-expert-project-master/core/src/main/java/com/di/CoreModule.kt
@@ -0,0 +1,55 @@
+package com.di
+
+import androidx.room.Room
+import com.data.TourismRepository
+import com.data.source.local.LocalDataSource
+import com.data.source.local.room.TourismDatabase
+import com.data.source.remote.RemoteDataSource
+import com.data.source.remote.network.ApiService
+import com.domain.repository.ITourismRepository
+import com.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+val databaseModule = module {
+    factory { get<TourismDatabase>().tourismDao() }
+    single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
+        Room.databaseBuilder(androidContext(), TourismDatabase::class.java, "Tourism")
+            .fallbackToDestructiveMigration().openHelperFactory(factory).build()
+    }
+}
+
+val networkModule = module {
+    single {
+        OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    single {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://tourism-api.dicoding.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(get())
+            .build()
+        retrofit.create(ApiService::class.java)
+    }
+}
+
+val repositoryModule = module {
+    single { com.data.source.local.LocalDataSource(get()) }
+    single { RemoteDataSource(get()) }
+    factory { AppExecutors() }
+    single<ITourismRepository> { com.data.TourismRepository(get(), get(), get()) }
+}
